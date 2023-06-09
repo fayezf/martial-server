4d5d1ab4182f310f2e7726c78b835fc12cc0f514
fayezf <mohammadfayezahmed835@gmail.com>
2023-06-10 00:44:17 +0600

    selected data added

--- index.js
+++ index.js
@@ -44,7 +44,17 @@ async function run() {
         res.send(result)
     })
 
-    // seats collection
+    // seats collection apis
+    app.get('/seats', async(req, res) => {
+        const email = req.query.email;
+        if(!email){
+            res.send([]);
+        }
+        const query = { email: email };
+        const result = await seatsCollection.find(query).toArray();
+        res.send(result)
+    })
+
     app.post('/seats', async(req, res) => {
         const item = req.body;
         const result = await seatsCollection.insertOne(item);
