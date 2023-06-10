3c4dc6f1b3b36302578c1f4cd3e455a7245cf6a8
fayezf <mohammadfayezahmed835@gmail.com>
2023-06-10 18:26:55 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -12,6 +13,22 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message: 'unauthorized access'});
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dt6wk0t.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,6 +51,14 @@ async function run() {
         const classesCollection = client.db("martialDb").collection("classes");
         const instructorsCollection = client.db("martialDb").collection("instructors");
         const seatsCollection = client.db("martialDb").collection("seats");
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
+
 
         // users related apis
         app.get('/users', async (req, res) => {
@@ -88,11 +113,17 @@ async function run() {
 
 
         // seats collection apis
-        app.get('/seats', async (req, res) => {
+        app.get('/seats', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([]);
             }
+
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail){
+                return res.status(403).send({error: true, message: 'forbidden access'})
+            }
+
             const query = { email: email };
             const result = await seatsCollection.find(query).toArray();
             res.send(result)
