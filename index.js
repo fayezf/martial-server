cd980d723a41ef993d6f7688d480a6dfee51325c
fayezf <mohammadfayezahmed835@gmail.com>
2023-06-10 10:31:47 +0600

    my classes data delete action

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -60,6 +60,13 @@ async function run() {
         const result = await seatsCollection.insertOne(item);
         res.send(result)
     })
+
+    app.delete('/seats/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await seatsCollection.deleteOne(query);
+        res.send(result)
+      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
